cb8adcb43feea2f3fb4febc773ee94914738d4a8
aldoalbuquerquejr <aldocandeia000@gmail.com>
2025-04-02 21:53:17 -0300

    Setting up front end environment

---
+++ Eventos-RCC/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Index.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='p-4 text-center text-xl font-bold'>Este é o início do projeto de uma aplicação web para organização de eventos da Renovação Carismática Católica da Paraíba!
+      </h1>
+    </>
+  )
+}
+
+
+export default App
